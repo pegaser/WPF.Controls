50a643bb34a8644899048ce89729dfa529230dc0
pegaser <sperezgabriel@gmail.com>
2022-10-13 16:45:46 -0500

    13_OCTUBRE_2022

---
+++ Homework/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows;
+
+namespace Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("This is the title", "Some message");
+        }
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    progress.IsActive = true;
+                    progress.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    progress.IsActive = false;
+                    progress.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
---
+++ Practice/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("This is the title", "Some message");
+        }
+    }
+}
